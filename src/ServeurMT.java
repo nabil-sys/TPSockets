3a55b4161d1fec4eb9105357061c5148e543dbcf
nabil-sys <boukhenaissi@et.esiea.fr>
2020-06-08 23:16:45 +0200

    Test client telnet (trouver un nombre inconnu)

--- src/ServeurMT.java
+++ src/ServeurMT.java
@@ -4,11 +4,15 @@ import java.net.Socket;
 
 public class ServeurMT extends Thread {
     int nbClients;
+    private int nombreSecret;
+    private boolean fin;
+    private String gagnant;
 
     @Override
     public void run() {
         try {
             ServerSocket ss = new ServerSocket(234);
+            nombreSecret =(int)(Math.random()*1000);
             while (true){
                 Socket s = ss.accept();
                 ++nbClients;
@@ -45,11 +49,33 @@ public class ServeurMT extends Thread {
 
                  System.out.println("Connexion du client numero"+numeroClients+"IP="+IP);
                  pw.println("Bienvenue, vous êtes le client numéro"+numeroClients);
+                 pw.println("Devinez le nombre secret entre 0 et 1000")
 
                  while (true){
                      String req;
                      while((req=br.readLine())!=null){
                          System.out.println(IP+"a envoyé:"+req);
+                         int nb=Integer.parseInt(req);
+                         if(fin==false){
+                             if(nb<nombreSecret){
+                                 pw.println("votre nombre est plus petit")
+                             }
+                             else if(nb>nombreSecret){
+                                 pw.println("votre nombre est plus grand")
+                             }
+                             else{
+                                 gagnant = IP;
+                                 fin = true;
+                                 pw.println("Bravo, vous avez gagné...")
+                             }
+                         }else{
+                             pw.println("Le jeu est terminé, le gagnant est :"+gagnant);
+                             System.out.println("**********");
+                             System.out.println("Bravo" +IP);
+                             System.out.println("**********");
+
+                         }
+
                          String rep = "Size="+req.length();
                          pw.println(rep);
                      }
